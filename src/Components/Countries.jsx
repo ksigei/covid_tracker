3179c99e00719713468601c7c8f944d26a2e8621
sigeikiprono4@gmail.com <sigeikiprono4@gmail.com>
2022-06-02 21:26:41 +0300

    Modified README.md file

--- src/Components/Countries.jsx
+++ src/Components/Countries.jsx
@@ -1,34 +1,60 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowCircleRight } from '@fortawesome/fontawesome-free-solid';
 import { getCountries } from '../Redux/Countries/actions';
+
 import './Sass/Countries.scss';
+import './Sass/SearchBar.scss';
 
 const Countries = () => {
+  const [query, setQuery] = useState('');
   const dispatch = useDispatch();
   const countries = useSelector((state) => state.countries);
   useEffect(() => {
     dispatch(getCountries());
   }, []);
   return (
-    <div className="container">
-      {countries.map((country) => (
-        <div key={country.id} className="card">
-          <div className="flex">
-            <h3>Country:</h3>
-            <h3 className="#">{country.country}</h3>
-          </div>
-          <img src={country.flag} alt="covid-images" />
-          <div className="flex">
-            <h3>Cases:</h3>
-            <h3 className="t_bg">{country.cases}</h3>
-          </div>
-          <div className="flex">
-            <NavLink to={`/country/${country.country}`}>Details</NavLink>
-          </div>
+    <>
+      <form
+        className="search-bar"
+        onChange={(event) => setQuery(event.target.value)}
+      >
+        <div className="search-bar__wrapper">
+          <input
+            className="input"
+            placeholder="Country Name"
+            // onChange={(event) => setQuery(event.target.value)}
+          />
         </div>
-      ))}
-    </div>
+      </form>
+      <div className="container">
+        {countries
+          .filter((country) => country.country.toLowerCase().includes(query.toLowerCase()))
+          .map((country) => (
+            <div key={country.id} className="card">
+              <div className="flex">
+                <h3>Country:</h3>
+                <h3 className="#">{country.country}</h3>
+              </div>
+              <img src={country.flag} alt="covid-images" />
+              <div className="flex">
+                <h3>Cases:</h3>
+                <h3 className="t_bg">{country.cases}</h3>
+              </div>
+              <div className="flex-end">
+                <NavLink to={`/country/${country.country}`}>
+                  <FontAwesomeIcon
+                    className="icons"
+                    icon={faArrowCircleRight}
+                  />
+                </NavLink>
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
   );
 };
 
--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getCountries } from '../Redux/Countries/actions';
-import './Sass/Countries.scss';
+import './Sass/Details.scss';
 
 const Details = () => {
   const { countryName } = useParams();
@@ -17,18 +17,50 @@ const Details = () => {
         <div key={country.id}>
           {country.country === countryName && (
             <div>
-              <div>
-                <h3>Country:</h3>
+              <div className="flex">
+                <h3 className="title">Country:</h3>
                 <h3 className="#">{country.country}</h3>
               </div>
               <img src={country.flag} alt="covid-images" />
-              <div>
-                <h3>Population:</h3>
+              <div className="flex">
+                <h3 className="title">Population:</h3>
                 <h3 className="t_bg">{country.population}</h3>
               </div>
-              <div>
-                <h3>Cases:</h3>
+              <div className="flex">
+                <h3 className="title">Cases:</h3>
                 <h3 className="t_bg">{country.cases}</h3>
+              </div>
+              <div className="flex">
+                <h3 className="title">Cases Today:</h3>
+                <h3 className="t_bg">{country.todayCases}</h3>
+              </div>
+              <div className="flex">
+                <h3 className="title">Deaths:</h3>
+                <h3 className="t_bg">{country.deaths}</h3>
+              </div>
+              <div className="flex">
+                <h3 className="title">Deaths Today:</h3>
+                <h3 className="t_bg">{country.todayDeaths}</h3>
+              </div>
+              <div className="flex">
+                <h3 className="title">Recovered:</h3>
+                <h3 className="t_bg">{country.recovered}</h3>
+              </div>
+              <div className="flex">
+                <h3 className="title">Recovered Today:</h3>
+                <h3 className="t_bg">{country.todayRecovered}</h3>
+              </div>
+              <div className="flex">
+                <h3 className="title">Active:</h3>
+                <h3 className="t_bg">{country.active}</h3>
+              </div>
+              <div className="flex">
+                <h3 className="title">Critical:</h3>
+                <h3 className="t_bg">{country.critical}</h3>
+              </div>
+              <div className="flex">
+                <h3 className="title">Tests:</h3>
+                <h3 className="t_bg">{country.tests}</h3>
               </div>
             </div>
           )}
