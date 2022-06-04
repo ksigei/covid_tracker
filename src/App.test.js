867a32db95c4fd499a4ac1b6616482f030d7558e
sigeikiprono4@gmail.com <sigeikiprono4@gmail.com>
2022-06-04 11:15:02 +0300

    Add tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import store from './Redux/store';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  test('renders', () => {
+    const tree = render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
 });
